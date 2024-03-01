dc74f1ca33e623fa677570bef1bac49807ad8bd2
vera <vera.filippova.79@mail.ru>
2024-03-01 20:43:21 +0300

    Avia added

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+        public static void main(String[] args){
+
+            int ticket, bounce;
+            ticket = 13676;
+            bounce = ticket / 20;
+
+            System.out.println("Бонусные мили : " + bounce);
+
+
+        }
+    }
+
